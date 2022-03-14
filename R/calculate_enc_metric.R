dce03d569109df2d77856e06b3d896daaaafce87
Lt Col Thomas A. Rehman, CAP/CPX <trehman@cap.gov>
2022-03-14 12:36:43 -0400

    Renaming the calculated items to scores rather than metrics, since the metrics language is already in use.

--- R/calculate_enc_metric.R
+++ R/calculate_enc_metric.R
@@ -1,18 +1,18 @@
-#' @title Calculate the Encampment Metric
-#' @description Calculate the encampment metric for all units in a Squadron of Merit Listing.
+#' @title Calculate the Encampment Score
+#' @description Calculate the encampment score for all units in a Squadron of Merit Listing.
 #' @details
-#' This metric awards 1 point for every first-time Encampment attendee.
+#' This score awards 1 point for every first-time Encampment attendee.
 #'
 #' The final result is then standardized using the distribution of the scores to have a mean of 1
 #' and standard deviation of 1.
 #' @param som_report A data.table containing the Squadron of Merit Listing data.
-#' @return A data.table containing the unit charter number and the encampment metric.
+#' @return A data.table containing the unit charter number and the encampment score.
 #' @export
 
-calculate_enc_metric <- function(som_report) {
+calculate_enc_score <- function(som_report) {
     result <- som_report[j = list(RWU, FrstEncCnt)]
-    result[j = enc_metric := scale(FrstEncCnt) + 1]
-    result <- result[j = list(RWU, enc_metric)]
+    result[j = enc_score := scale(FrstEncCnt) + 1]
+    result <- result[j = list(RWU, enc_score)]
     result[]
     return(result)
 }
--- R/calculate_flight_metric.R
+++ R/calculate_flight_metric.R
@@ -1,22 +1,22 @@
-#' @title Calculate the Flight Metric
-#' @description Calculate the flight metric for all units in a Squadron of Merit Listing.
+#' @title Calculate the Flight Score
+#' @description Calculate the flight score for all units in a Squadron of Merit Listing.
 #' @details
-#' This metric is calculated by multiplying the Flight99 measure by 1, awarding 1 point for a value
+#' This score is calculated by multiplying the Flight99 measure by 1, awarding 1 point for a value
 #' of 100% on the Flight99 measure. The point value is doubled if the orientation flights flag
 #' is set to Yes.
 #'
 #' The final result is then standardized using the distribution of the scores to have a mean of 1
 #' and standard deviation of 1.
 #' @param som_report A data.table containing the Squadron of Merit Listing data.
-#' @return A data.table containing the unit charter number and the flight metric.
+#' @return A data.table containing the unit charter number and the flight score.
 #' @export
 
-calculate_flight_metric <- function(som_report) {
+calculate_flight_score <- function(som_report) {
     result <- som_report[j = list(RWU, OFlights, Flight99)]
     result[j = multiplier := 1]
     result[OFlights == "YES", multiplier := 2]
-    result[j = flight_metric := scale((Flight99 * multiplier) / 100) + 1]
-    result <- result[j = list(RWU, flight_metric)]
+    result[j = flight_score := scale((Flight99 * multiplier) / 100) + 1]
+    result <- result[j = list(RWU, flight_score)]
     result[]
     return(result)
 }
